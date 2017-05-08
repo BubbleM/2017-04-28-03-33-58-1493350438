78d28396410d80e3e4a88882397c7e5bdc3ea9f1
BuddleM <1870408473@qq.com>
2017-05-09 01:17:55 +0800

    再次检查 删除多余测试输出代码 添加注释

--- src/草稿.js
+++ src/草稿.js
@@ -1,28 +1,49 @@
 function getBowlingScore(bowlingCode){
 	var extraIndex = bowlingCode.indexOf("||");//返回||在字符串中首次出现的位置
+	// print(extraIndex);
 	var	extra1 = parseInt(judgeNum(bowlingCode.substring(extraIndex+2,extraIndex+3)));//额外第一球机会的击倒球数
 	var extra2 = parseInt(judgeNum(bowlingCode.substring(extraIndex+3)));//额外第二球的击倒球数
 	var bowling = bowlingCode.substring(0,extraIndex);//前10次的情况
+	// print();
+	// print(typeof(extra1));
+	// print(extra2);
+	// print(bowling);
 	var bowlingArr = bowling.split('|');//将前10次的情况保存在数组中
 	var i = [];
 	var temp = [];
+	// print(bowlingArr);
 	for(var i=0;i<bowlingArr.length;i++){
 		temp[i] = [];
 		for(var j=0;j<2;j++){
 			temp[i][j] = parseInt(judgeNum(bowlingArr[i][j]));//将前10次情况转换为数值成绩
 		}
 	}
+	// print(temp);
 	bowlingArr = temp; 
+	// print(bowlingArr);
+	// for(var i=0;i<bowlingArr.length;i++){
+	// 	print(bowlingArr[i]);
+	// }
+
 	var scores = [0,0,0,0,0,0,0,0,0,0];//每局的得分
 	var totalScore = 0;//总成绩
 	var firstHit = true;//是否第一次击球
+
+
+	var firstHit = 0;
+	var secondHit = 0;
 	for(var i=0;i<bowlingArr.length;i++){
 		if(bowlingArr[i][1] == -1){
-			bowlingArr[i][1] = 10 - bowlingArr[i][0]; //如果第二次补中，将/转换为对应成绩
+				bowlingArr[i][1] = 10 - bowlingArr[i][0];
 		}
-		firstHit = bowlingArr[i][0]; //每局第一次击中成绩
-		if(firstHit < 10){ //如果第一局全中，该局结束
-			secondHit = bowlingArr[i][1];//每局第二次击中成绩
+		// print("=====第" + (i+1) +"回合=====");
+		firstHit = bowlingArr[i][0];
+		// print("第一次击中：" + firstHit);
+		// throwTheBall(firstHit);
+		if(firstHit < 10){
+			secondHit = bowlingArr[i][1];
+			// print("第二次击中：" + secondHit);
+			// throwTheBall(secondHit);
 		}
 		if(i == bowlingArr.length-1){
 			if(firstHit == 10 || firstHit+secondHit == 10){
@@ -36,6 +57,8 @@ function getBowlingScore(bowlingCode){
 		}
 	}
 	calcScore();//计算成绩
+	// print("总分：" + getTotalScore());
+
 	function calcScore(){
 		for(var i=0;i<bowlingArr.length-1;i++){//第1格到第9格
 			if(bowlingArr[i][0] == 10){//strike 第一次就全中
@@ -76,7 +99,8 @@ function getBowlingScore(bowlingCode){
 
 	function getTotalScore(){ //总成绩
 		for(var i=0;i<scores.length;i++){
-			// print(i + '=' + scores[i]);//输出每局成绩
+			// print(i + '=' + scores[i]);
+			// print(typeof(scores[i]));
 			totalScore += scores[i];
 		}
 		return totalScore;
